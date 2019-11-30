97d8ddadaf19487daf9fd0a7f976f991de528ba9
Andy Marks <vampwillow@gmail.com>
2019-11-30 21:04:54 +1100

    +custom square sizer fn

---
+++ src/square-sizer/handler.spec.ts
@@ -0,0 +1,16 @@
+import { handler } from "./handler";
+
+const expect = require("chai").expect;
+
+describe("handler", () => {
+  describe("handler", () => {
+    it("should return Serverless boilerplate message", () => {
+      handler(null, null, (error: Error, result: any) => {
+        expect(error).to.be.null;
+        expect(result.body).to.equal(
+          '{"message":"square-sizer","input":null}'
+        );
+      });
+    });
+  });
+});
---
+++ src/square-sizer/handler.ts
@@ -0,0 +1,18 @@
+import { APIGatewayEvent, Context, Handler, Callback } from "aws-lambda";
+
+export const handler: Handler = (
+  event: APIGatewayEvent,
+  _: Context,
+  cb: Callback
+) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message:
+        "square-sizer",
+      input: event
+    })
+  };
+
+  cb(null, response);
+};
