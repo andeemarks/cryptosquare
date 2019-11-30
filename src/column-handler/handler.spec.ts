36ebb922ac2231d0ef711a01fd78c9aec9edfa44
Andy Marks <vampwillow@gmail.com>
2019-12-01 09:18:55 +1100

    Carry handler->fn across tests

--- src/column-handler/handler.spec.ts
+++ src/column-handler/handler.spec.ts
@@ -1,11 +1,11 @@
-import { handler } from "./handler";
+import { fn } from "./handler";
 
 const expect = require("chai").expect;
 
 describe("handler", () => {
-  describe("handler", () => {
+  describe("fn", () => {
     it("should return Serverless boilerplate message", () => {
-      handler(null, null, (error: Error, result: any) => {
+      fn(null, null, (error: Error, result: any) => {
         expect(error).to.be.null;
         expect(result.body).to.equal(
           '{"message":"column-handler","input":null}'
--- src/square-sizer/handler.spec.ts
+++ src/square-sizer/handler.spec.ts
@@ -1,11 +1,11 @@
-import { handler } from "./handler";
+import { fn } from "./handler";
 
 const expect = require("chai").expect;
 
-describe("handler", () => {
-  describe("handler", () => {
+describe("square-sizer", () => {
+  describe("fn", () => {
     it("should return Serverless boilerplate message", () => {
-      handler(null, null, (error: Error, result: any) => {
+      fn(null, null, (error: Error, result: any) => {
         expect(error).to.be.null;
         expect(result.body).to.equal(
           '{"message":"square-sizer","input":null}'
