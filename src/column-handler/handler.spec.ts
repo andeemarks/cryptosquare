ec74aa11a05355eedf9ad61ba1262c270f5d8d75
Andy Marks <vampwillow@gmail.com>
2019-12-01 17:54:39 +1100

    First real implementation of column-handler

--- src/column-handler/handler.spec.ts
+++ src/column-handler/handler.spec.ts
@@ -1,15 +1,34 @@
 import { fn } from "./handler";
+import { APIGatewayEvent } from "aws-lambda";
+const utils = require("aws-lambda-test-utils");
+const event: APIGatewayEvent = utils.mockEventCreator.createAPIGatewayEvent();
 
 const expect = require("chai").expect;
 
 describe("handler", () => {
   describe("fn", () => {
     it("should return Serverless boilerplate message", () => {
-      fn(null, null, (error: Error, result: any) => {
+      event.pathParameters.plaintext = "abcd";
+      event.pathParameters.columncount = "2";
+      fn(event, null, (error: Error, result: any) => {
         expect(error).to.be.null;
-        expect(result.body).to.equal(
-          '{"message":"column-handler","input":null}'
-        );
+        expect(result.body).to.equal('["ac","bd"]');
+      });
+    });
+    it("should return the plaintext unchanged with a 1 columncount", () => {
+      event.pathParameters.plaintext = "abcdef";
+      event.pathParameters.columncount = "1";
+      fn(event, null, (error: Error, result: any) => {
+        expect(error).to.be.null;
+        expect(result.body).to.equal('["abcdef"]');
+      });
+    });
+    it("should add rows if the plaintext is too short", () => {
+      event.pathParameters.plaintext = "a";
+      event.pathParameters.columncount = "2";
+      fn(event, null, (error: Error, result: any) => {
+        expect(error).to.be.null;
+        expect(result.body).to.equal('["a"," "]');
       });
     });
   });
--- src/column-handler/handler.ts
+++ src/column-handler/handler.ts
@@ -1,17 +1,36 @@
 import { APIGatewayEvent, Context, Handler, Callback } from "aws-lambda";
+import * as _ from "lodash";
+
+let plaintextToColumns = (plaintext: string, columnCount: number): string[] => {
+  if (columnCount == 1) {
+    return [plaintext];
+  }
+
+  const charsByRemainder = _.map(plaintext.split(""), (element, position,) => { let foo = {} ; foo[(position % columnCount).toString()] = element ; return foo });
+  const sortedCharsByRemainder = _.sortBy(charsByRemainder, (item) => {return Object.keys(item)[0]})
+  const sortedChars = _.map(sortedCharsByRemainder, (item) => {return Object.values(item)[0]})
+
+  const chunkedSortedChars = sortedChars.join("").match(/.{1,2}/g);
+
+  if (chunkedSortedChars.length < columnCount) {
+    chunkedSortedChars.push(" ")
+  }
+  
+  return chunkedSortedChars;
+};
 
 export const fn: Handler = (
   event: APIGatewayEvent,
   _: Context,
   cb: Callback
 ) => {
+  const result = plaintextToColumns(
+    event.pathParameters.plaintext,
+    new Number(event.pathParameters.columncount).valueOf()
+  );
   const response = {
     statusCode: 200,
-    body: JSON.stringify({
-      message:
-        "column-handler",
-      input: event
-    })
+    body: JSON.stringify(result)
   };
 
   cb(null, response);
