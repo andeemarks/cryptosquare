5998271002273be71ba850fd17001a2e601b21d9
Andy Marks <vampwillow@gmail.com>
2019-12-02 11:23:28 +1100

    Only one test, so setup can be contained

--- src/column-handler/handler.spec.ts
+++ src/column-handler/handler.spec.ts
@@ -1,13 +1,12 @@
 import { fn } from "./handler";
 import { APIGatewayEvent } from "aws-lambda";
 const utils = require("aws-lambda-test-utils");
-const event: APIGatewayEvent = utils.mockEventCreator.createAPIGatewayEvent();
-
 const expect = require("chai").expect;
 
 describe("handler", () => {
   describe("#fn", () => {
     it("should operate on pathParameters", () => {
+      const event: APIGatewayEvent = utils.mockEventCreator.createAPIGatewayEvent();
       event.pathParameters.plaintext = "abcd";
       event.pathParameters.columncount = "2";
       fn(event, null, (error: Error, result: any) => {
